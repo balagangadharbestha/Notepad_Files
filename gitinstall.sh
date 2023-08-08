4144b92e2ce3d63dd27dc3c5100d5e595f4f9b1c
balagangadharbestha <besthagangadhar000@gmail.com>
2023-08-08 11:05:41 +0000

    fullymodified

--- gitinstall.sh
+++ gitinstall.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-yum install autoconf cpio curl-devel expat-devel gcc gettext-devel make openssl-devel perl-ExtUtils-MakeMaker zlib-devel -y
-wget -O https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.4.3.tar.gz
+set -e
+
+VERSION="2.32.7"
+
+# Check and install dependencies
+sudo yum install autoconf cpio curl-devel expat-devel gcc gettext-devel make openssl-devel perl-ExtUtils-MakeMaker zlib-devel -y
 sudo yum install wget -y
-wget  https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.4.12.tar.gz
-tar -xzvf git-2.4.12.tar.gz
-cd git-2.4.12/
+
+# Download and install Git
+wget  https://mirrors.edge.kernel.org/pub/software/scm/git/git-$VERSION.tar.gz
+tar -xzvf git-$VERSION.tar.gz
+cd git-$VERSION/
 make configure
 ./configure --prefix=/usr/local/git
-make && make install
-ln -sf /usr/local/git/bin/* /usr/bin/
+make && sudo make install
+
+# Create symbolic links
+sudo ln -sf /usr/local/git/bin/* /usr/bin/
+
+# Display Git version
 git --version
+
+# Clean up
+cd ..
+rm -rf git-$VERSION git-$VERSION.tar.gz
 
